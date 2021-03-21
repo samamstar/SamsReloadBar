2459c0049cdbeb2ae69f1004d96535bd6c72e12d
samamstar <samamstar@gmail.com>
2021-03-20 18:12:39 -0600

    Turns out that c++ functions don't work. RIP
    It's like baked into the exe or something, so the game crashes when you try to use one

---
+++ Source/SamsReloadBar/SamsReloadBar.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SamsReloadBar.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SamsReloadBar, "SamsReloadBar" );
